98228212667fcdd06068ff3ee02edee59b24bcb7
saketkum11 <kumarsaket601@gmail.com>
2022-03-21 22:36:07 +0530

    added:cart

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import logo from "./logo.png";
-
-
+import {Routes,Route,Link} from "react-router-dom"
+import { Home } from "./Pages/Home/home";
+import {Cart} from "./Pages/Cart/cart";
 function App() {
   return (
     <div className="App">
-     
-      
+     <nav>
+       <Link to="/">Home</Link>
+       <Link to="/product">Product</Link>
+       <Link to="/cart">Cart</Link>
+
+     </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/product" element={<Product/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
+      </Routes>
     </div>
   );
 }
