25456f92a2a31f537f7b823b84bdff0fa52e33d0
saketkum11 <kumarsaket601@gmail.com>
2022-03-21 22:22:52 +0530

    added:routes

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import {BrowserRoute} from "react-router-dom"
 
+import {BrowserRouter} from "react-router-dom"
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRoute>
-    <App />
-    </BrowserRoute>
+  <BrowserRouter>  
+     <App />
+   </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
