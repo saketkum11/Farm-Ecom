444df117ccd80b11ab7b2cd33279a73b93642a97
saketkum11 <kumarsaket601@gmail.com>
2022-03-21 22:05:06 +0530

    added:Links

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import logo from "./logo.png";
-
+import {Routes,Route} from "react-router-dom"
+import {Link}  from "react-router-dom"
+import Home from "./Pages/Home/Home.jsx"
 function App() {
   return (
     <div className="App">
-      
+      <nav>
+        <Link to="/">Home</Link>
+
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
     </div>
   );
 }
