4205c207cd4600eac30929d32ed3fa7ec7fa1800
saketkum11 <kumarsaket601@gmail.com>
2022-03-22 13:37:29 +0530

    added:assests

--- ecommerce/src/Pages/Home/home.jsx
+++ ecommerce/src/Pages/Home/home.jsx
@@ -3,15 +3,11 @@ import { NavBar } from "../../component/Navbar/Navbar";
 export function Home(){
 return (
 <main class="wt-80 m-auto ">
-
-    
     <div class="flex items-start justify-center h-lg m-y-11 position-rel">
         <img src="/css/image/Egg.jpg" alt="" class="rounded-L object-content wt-100 h-100"/>
         <a href="/component/product/product.html" target="_blank"
             class="text-color-9 text-dec text-m rounded-m pd-x-9 pd-y-3 position-ab bg-black-1 p-t-7 p-lf-1">More</a>
     </div>
-
-   
     <div class="m-y-9">
         <span class="text-lg text-bold">Categories</span>
     </div>
@@ -43,11 +39,6 @@ return (
             <span class="position-ab  text-color-0 text-m text-bold p-t-5">Fruits</span>
         </div>
     </section>
-
-
-
-
-
     <section class="m-y-11">
         <div class="m-y-11">
             <span class="text-lg text-bold">Coming Soon</span>
